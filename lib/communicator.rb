185cf01049b19acccabbd51c4e79fa9bfcb9df07
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2009-10-14 17:00:32 -0600

    Initial addition of the communicator with its certificate authority for ssl comunications and rspec tests.

---
+++ lib/communicator.rb
@@ -0,0 +1,85 @@
+require 'net/https'
+require 'uri'
+
+class FsCommunicator
+  attr_accessor :domain, :key, :user_agent, :session
+  
+  def initialize(options = {})
+    # merge default options with options hash
+    o = {
+      :domain => 'http://www.dev.usys.org',
+      :key => '',
+      :user_agent => 'FsCommunicator/0.1 (Ruby)', # should be overridden by options user_agent
+      :session => nil
+    }.merge(options)
+    @domain = o[:domain]
+    @key = o[:key]
+    @user_agent = o[:user_agent]
+    @session = o[:session]
+  end
+  
+  def post(url,payload)
+    uri = URI.parse(self.domain+url)
+    full_url = set_extra_params(uri)
+    request = Net::HTTP::Post.new(full_url)
+    request.body = payload
+    request['Content-Type'] = "application/json"
+    request['User-Agent'] = self.user_agent
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true 
+      http.ca_file = File.join File.dirname(__FILE__), 'assets','entrust-ca.crt'
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+    res = http.start do |ht|
+      ht.request(request)
+    end
+  end
+  
+  def get(url,credentials = {})
+    uri = URI.parse(self.domain+url)
+    full_url = set_extra_params(uri,credentials)
+    request = Net::HTTP::Get.new(full_url)
+    request['User-Agent'] = self.user_agent
+    if credentials[:username] && credentials[:password]
+      request.basic_auth credentials[:username], credentials[:password]
+    end
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true 
+      http.ca_file = File.join File.dirname(__FILE__), 'assets','entrust-ca.crt'
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+    res = http.start do |ht|
+      ht.request(request)
+    end
+  end
+  
+  private
+  def set_extra_params(uri,credentials = {})
+    if credentials[:username] && credentials[:password]
+      sessionized_url = add_key(uri)
+    else
+      sessionized_url = add_session(uri)
+    end
+    sessionized_url << '&dataFormat=application/json'
+  end
+  
+  def add_session(uri)
+    if uri.query
+      uri.query << '&sessionId=' + self.session
+    else
+      uri.query = 'sessionId=' + self.session
+    end
+    uri.request_uri
+  end
+  
+  def add_key(uri)
+    if uri.query
+      uri.query << '&key=' + self.key
+    else
+      uri.query = 'key=' + self.key
+    end
+    uri.request_uri
+  end
+end
---
+++ spec/communicator_spec.rb
@@ -0,0 +1,191 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'communicator'
+
+module HttpCommunicatorHelper
+  
+  def stub_net_objects
+    @request = mock("Net::HTTP::Get|Post")
+    @request.stub!(:[]=)
+    @request.stub!(:body=)
+    @http = mock("Net::HTTP")
+    Net::HTTP.stub!(:new).and_return(@http)
+    Net::HTTP::Get.stub!(:new).and_return(@request)
+    Net::HTTP::Post.stub!(:new).and_return(@request)
+    @http.stub!(:use_ssl=)
+    @http.stub!(:ca_file=)
+    @http.stub!(:verify_mode=)
+    @http.stub!(:start)
+  end
+  
+end
+
+describe FsCommunicator do
+  include HttpCommunicatorHelper
+  describe "initializing" do
+    it "should accept a hash of options" do
+      lambda {
+        com = FsCommunicator.new :domain => 'https://api.familysearch.org', :key => '1111-1111', :user_agent => "FsCommunicator/0.1"
+      }.should_not raise_error
+    end
+    
+    it "should set defaults to the Reference System" do
+      com = FsCommunicator.new
+      com.domain.should == 'http://www.dev.usys.org'
+      com.key.should == ''
+      com.user_agent.should == 'FsCommunicator/0.1 (Ruby)'
+    end
+    
+    it "should set the domain, key, and user_agent to options hash" do
+      options = {
+        :domain => 'https://api.familysearch.org', 
+        :key => '1111-1111', 
+        :user_agent => "RSpecTest/0.1",
+        :session => 'SESSID'
+      }
+      com = FsCommunicator.new options
+      com.domain.should == options[:domain]
+      com.key.should == options[:key]
+      com.user_agent.should == options[:user_agent]
+      com.session.should == options[:session]
+    end
+  end
+  
+  describe "GET on a URL" do
+    before(:each) do
+      options = {
+        :domain => 'https://api.familysearch.org', 
+        :key => '1111-1111', 
+        :user_agent => "FsCommunicator/0.1",
+        :session => 'SESSID'
+      }
+      @com = FsCommunicator.new options
+      stub_net_objects
+      @url = '/familytree/v1/person/KWQS-BBQ'
+      @session_url = @url + "?sessionId=#{@com.session}&dataFormat=application/json"
+    end
+    
+    def do_get(url, credentials = {})
+      @com.get(url, credentials)
+    end
+    
+    it "should initialize a Net::HTTP object to make the request" do
+      Net::HTTP.should_receive(:new).with('api.familysearch.org',443).and_return(@http)
+      do_get(@url)
+    end
+    
+    it "should create a GET request with url containing a session" do
+      Net::HTTP::Get.should_receive(:new).with(@session_url).and_return(@request)
+      do_get(@url)
+    end
+    
+    it "should tack a sessionId as an additional parameter if params already set" do
+      url = "/familytree/v1/person/KWQS-BBQ?view=summary"
+      Net::HTTP::Get.should_receive(:new).with(url+"&sessionId=#{@com.session}&dataFormat=application/json").and_return(@request)
+      do_get(url)
+    end
+    
+    it "should set the http object to use ssl if https" do
+      @http.should_receive(:use_ssl=).with(true)
+      do_get(@url)
+    end
+    
+    it "should not set the http object to use ssl if no http" do
+      @com.domain = 'http://www.dev.usys.org'
+      @http.should_not_receive(:use_ssl=)
+      do_get(@url)
+    end
+    
+    it "should set the ca file to the entrust certificate (for FamilySearch systems)" do
+      @http.should_receive(:ca_file=).with(File.join(File.dirname(__FILE__),'..','lib','assets','entrust-ca.crt'))
+      do_get(@url)
+    end
+    
+    it "should set the basic_authentication if the credentials passed as parameters" do
+      @request.should_receive(:basic_auth).with('user','pass')
+      @request.should_receive(:[]=).with('User-Agent',@com.user_agent)
+      Net::HTTP::Get.should_receive(:new).with(@url+"?key=#{@com.key}&dataFormat=application/json").and_return(@request)
+      do_get(@url,:username => 'user',:password => 'pass')
+    end
+    
+    it "should make the request" do
+      block = lambda{ |ht|
+        ht.request('something')
+      }
+      @http.should_receive(:start)
+      do_get(@url)
+    end
+    
+  end
+  
+  describe "POST on a URL" do
+    before(:each) do
+      options = {
+        :domain => 'https://api.familysearch.org', 
+        :key => '1111-1111', 
+        :user_agent => "FsCommunicator/0.1",
+        :session => 'SESSID'
+      }
+      @com = FsCommunicator.new options
+      stub_net_objects
+      @url = '/familytree/v1/person/KWQS-BBQ'
+      @session_url = @url + "?sessionId=#{@com.session}&dataFormat=application/json"
+      @payload = "<familytree></familytree>"
+    end
+    
+    def do_post(url, payload = '')
+      @com.post(url, payload)
+    end
+    
+    it "should initialize a Net::HTTP object to make the request" do
+      Net::HTTP.should_receive(:new).with('api.familysearch.org',443).and_return(@http)
+      do_post(@url)
+    end
+    
+    it "should create a POST request with url containing a session" do
+      Net::HTTP::Post.should_receive(:new).with(@session_url).and_return(@request)
+      do_post(@url)
+    end
+    
+    it "should tack a sessionId as an additional parameter if params already set" do
+      url = "/familytree/v1/person/KWQS-BBQ?view=summary"
+      Net::HTTP::Post.should_receive(:new).with(url+"&sessionId=#{@com.session}&dataFormat=application/json").and_return(@request)
+      do_post(url)
+    end
+    
+    it "should set the request's body to the payload attached" do
+      @request.should_receive(:body=).with(@payload)
+      do_post(@url,@payload)
+    end
+    
+    it "should set the request's Content-Type to application/json" do
+      @request.should_receive(:[]=).with('Content-Type','application/json')
+      do_post(@url,@payload)
+    end
+    
+    it "should set the http object to use ssl if https" do
+      @http.should_receive(:use_ssl=).with(true)
+      do_post(@url)
+    end
+    
+    it "should not set the http object to use ssl if no http" do
+      @com.domain = 'http://www.dev.usys.org'
+      @http.should_not_receive(:use_ssl=)
+      do_post(@url)
+    end
+    
+    it "should set the ca file to the entrust certificate (for FamilySearch systems)" do
+      @http.should_receive(:ca_file=).with(File.join(File.dirname(__FILE__),'..','lib','assets','entrust-ca.crt'))
+      do_post(@url)
+    end
+        
+    it "should make the request" do
+      block = lambda{ |ht|
+        ht.request('something')
+      }
+      @http.should_receive(:start)
+      do_post(@url)
+    end
+    
+  end
+  
+end
