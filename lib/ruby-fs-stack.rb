ddd5855b216b009dd56d0d5bafb81379b836e66f
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2010-04-07 19:11:07 -0600

    Added a RubyFsStack.version method which pulls the version from the VERSION file (and caches it in memory).

--- lib/ruby-fs-stack.rb
+++ lib/ruby-fs-stack.rb
@@ -1,2 +1,8 @@
 require 'ruby-fs-stack/identity'
 require 'ruby-fs-stack/familytree'
+
+module RubyFsStack
+  def self.version
+    @@version ||= File.read(File.join(File.dirname(__FILE__),'..','VERSION')).strip
+  end
+end
--- spec/ruby-fs-stack_spec.rb
+++ spec/ruby-fs-stack_spec.rb
@@ -1,6 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "RubyFsStack" do
-  it "passes" do
+describe RubyFsStack do
+  describe "version" do
+    before(:each) do
+      
+      @version = File.read(File.join(File.dirname(__FILE__),'..','VERSION')).strip
+    end
+    
+    it "should read the version from the file" do
+      RubyFsStack.version.should == @version
+    end
   end
 end
